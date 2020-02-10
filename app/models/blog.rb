e43201224135e58e77b46f6d5fe106747d229dc2
Sinthorn Pradutnam <psinthorn@gmail.com>
2020-02-10 19:09:41 +0700

    Add validates title body and validate image type to blog

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -7,4 +7,19 @@ class Blog < ApplicationRecord
 
   # *upload multi image
   # has_many_attached :images
+
+  validates :title, :body, presence: true
+  validate :imageType
+  # validates :body, presence: true
+
+  private
+  def imageType
+    if image.attached? == false
+      errors.add(:image, "can't be blank")
+    else 
+      if !image.content_type.in?(%('image/jpeg image/png'))
+      errors.add(:image, 'needs to be JPEG or PNG')
+      end
+    end    
+  end
 end
